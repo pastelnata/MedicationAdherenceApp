5ebdcb43c1a08335dfb5ce1c22db4216da940dad
Sandra Gallmayer <sandrag@outlook.dk>
2025-10-23 16:38:46 +0200

    merge login, header, and sidebar with mainpage medicine boxes and new file structure

--- app/src/main/java/com/example/medicationadherenceapp/LoginPage.kt
+++ app/src/main/java/com/example/medicationadherenceapp/LoginPage.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
+//import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.*
