84be78fc99061db7342d10e2b688a05d56f40858
habib <habibwasi9@gmail.com>
2025-10-25 15:44:37 +0200

    alert icon rollback

--- app/src/main/java/com/example/medicationadherenceapp/MainPage.kt
+++ app/src/main/java/com/example/medicationadherenceapp/MainPage.kt
@@ -8,8 +8,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
@@ -17,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,6 +49,12 @@ fun MainPage() {
                 .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
+            Icon(
+                painter = painterResource(DrawableIcons.ALARM.id),
+                contentDescription = "Alert",
+                modifier = Modifier.size(30.dp).padding(end = 4.dp),
+                tint = Color(0xFFD32F2F)
+            )
             HeaderText("Needs Immediate Attention")
         }
 
